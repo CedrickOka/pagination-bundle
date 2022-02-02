be0f19dc4544295175d3ed36106e770697b42f5a
Cedrick Oka Baidai <cedric.baidai@veone.net>
2022-02-02 15:22:33 +0000

    Added cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,15 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('docs')
+    ->exclude('var')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+    ])
+    ->setFinder($finder)
+    ->setCacheFile('.php-cs-fixer.cache') // forward compatibility with 3.x line
+;
