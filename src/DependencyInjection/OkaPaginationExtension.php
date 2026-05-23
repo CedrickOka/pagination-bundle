6710a5d563aeace4d393df275fe2148d1823ce8c
Cedric Ceruya Oka Baidai <cedric.baidai@aynid.io>
2026-05-23 21:08:24 +0000

    Fixed composer dependencies

--- src/DependencyInjection/OkaPaginationExtension.php
+++ src/DependencyInjection/OkaPaginationExtension.php
@@ -5,8 +5,8 @@ namespace Oka\PaginationBundle\DependencyInjection;
 use Oka\PaginationBundle\Pagination\FilterExpression\FilterExpressionInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
